c17dbdbbe0a3ff09bf055e2ac0be5cd7780bc834
mahamed2694 <mahamed.nehme@daxia.com.br>
2026-05-31 20:10:01 -0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
+  experimental: {
+    forceSwcTransforms: false,
+  },
+  swcMinify: false,
 }
 
 module.exports = nextConfig
